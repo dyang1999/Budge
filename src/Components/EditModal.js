2d04f3325423a2e8ff12c7848a11db7b608515da
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-02-27 00:14:58 +0800

    edits

--- src/Components/EditModal.js
+++ src/Components/EditModal.js
@@ -5,25 +5,42 @@ import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function EditModal(props) {
-
   const [input, setInput] = useState(0);
 
   const onEditHandler = () => {
-    props.onEditHandler(props.day, input)
-    props.onClose()
-    props.onGrey(true)
-  }
+    props.onEditHandler(props.day, input);
+    props.onClose();
+    props.onGrey(true);
+  };
 
   return (
     <div className={classes.root}>
-      <input
-        className={classes.searchBar}
-        value={input}
-        onInput={(e) => setInput(e.target.value)}
-        type="text"
-      />
-      <Button onClick={onEditHandler}>Edit</Button>
-      <Button onClick={props.onClose}>Close</Button>
+      <div className={classes.inline}>
+        <h1 className={classes.text}>$</h1>
+        <input
+          className={classes.searchBar}
+          value={input}
+          onInput={(e) => setInput(e.target.value)}
+          type="text"
+        />
+      </div>
+      <div className={classes.inline1}>
+        <Button
+          variant="outline-primary"
+          onClick={onEditHandler}
+          className={classes.btn}
+          style={{marginRight: "15px"}}
+        >
+          Edit
+        </Button>
+        <Button
+          variant="outline-danger"
+          onClick={props.onClose}
+          className={classes.btn}
+        >
+          Close
+        </Button>
+      </div>
     </div>
   );
 }
